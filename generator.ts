8afed98eba54c814ec5191f1158cf63d8bd461ae
Jonas Pauli <49498646+jonas089@users.noreply.github.com>
2023-08-11 21:22:27 +0200

    estimate time in hours

--- generator.ts
+++ generator.ts
@@ -82,7 +82,8 @@ function entryPoint(){
     }
     else if (action == 'estimate'){
         let estimation = estimate(generatorLimit, publicSuffix);
-        console.log(`To generate a Public Key that ends with "${publicSuffix}" will take an est. of ${estimation.attempts} attempts and ${estimation.time} ms! Only run the generator if this sound feasible.`);
+        let estimationTimeInHours = estimation.time / 1000 / 60 / 60;
+        console.log(`To generate a Public Key that ends with "${publicSuffix}" will take an est. of ${estimation.attempts} attempts and ${estimationTimeInHours} hours on your machine! Only run the generator if this sound feasible.`);
     }
 }
 
